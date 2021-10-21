ead87306cd42b38d84059060970bab5f9d4e7463
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-10-21 09:01:47 -0300

    Initial Commit

---
+++ swift-051-get_all_weeks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-051-get_all_weeks
+//
+//  Created by Luiz Araujo on 21/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
